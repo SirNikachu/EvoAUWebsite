98041e86322244f2f0dd8275f37475c1f9e2caa6
SirNikachu <63741951+SirNikachu@users.noreply.github.com>
2025-10-14 02:30:52 -0500

    v1

---
+++ team-data.js
@@ -0,0 +1,128 @@
+const teamData = [
+    {
+    name: "Faisal Ahmed",
+    role: "Graduate Researcher",
+    university: "Auburn University, Engineering",
+    photo: "https://eng.auburn.edu/images/image-not-available.jpg",
+    email: "fza0062@auburn.edu",
+    phone: "3348443532",
+    website: "https://nepcm.eng.auburn.edu/news.php"
+  },
+  {
+    name: "Jack Shanahan",
+    role: "Graduate Researcher",
+    university: "Auburn University, Engineering",
+    photo: "https://eng.auburn.edu/images/image-not-available.jpg",
+    email: "jts0143@auburn.edu",
+    phone: "3348443532",
+    website: "https://nepcm.eng.auburn.edu/news.php"
+  },
+  {
+    name: "Tommy Kratt",
+    role: "Graduate Researcher",
+    university: "Auburn University, Engineering",
+    photo: "https://eng.auburn.edu/images/image-not-available.jpg",
+    email: "trk0028@auburn.edu",
+    phone: "3348443532",
+    website: "https://nepcm.eng.auburn.edu/news.php"
+  },
+  {
+    name: "Bikram Bhattarai",
+    role: "Graduate Researcher",
+    university: "Auburn University, Engineering",
+    photo: "https://eng.auburn.edu/images/image-not-available.jpg",
+    email: "bikram.bhattarai@auburn.edu",
+    phone: "3348443532",
+    website: "https://nepcm.eng.auburn.edu/news.php"
+  },
+  {
+    name: "Ben Tzou",
+    role: "Computational Modeling",
+    university: "Auburn University, Engineering",
+    photo: "https://eng.auburn.edu/images/image-not-available.jpg",
+    email: "bzt0035@auburn.edu",
+    phone: "3348443532",
+    website: "https://nepcm.eng.auburn.edu/news.php"
+  },
+  {
+    name: "Daniel Vallejo",
+    role: "Computational Modeling",
+    university: "Auburn University, Engineering",
+    photo: "https://eng.auburn.edu/images/image-not-available.jpg",
+    email: "dzv0027@auburn.edu",
+    phone: "3348443532",
+    website: "https://nepcm.eng.auburn.edu/news.php"
+  },
+    {
+    name: "George Crocket",
+    role: "Computational Modeling",
+    university: "Auburn University, Engineering",
+    photo: "https://eng.auburn.edu/images/image-not-available.jpg",
+    email: "gic0006@auburn.edu",
+    phone: "3348443532",
+    website: "https://nepcm.eng.auburn.edu/news.php"
+  },
+    {
+    name: "Alexandra Warhurst ",
+    role: "Hardware",
+    university: "Auburn University, Engineering",
+    photo: "https://eng.auburn.edu/images/image-not-available.jpg",
+    email: "anw0081@auburn.edu",
+    phone: "3348443532",
+    website: "https://nepcm.eng.auburn.edu/news.php"
+  },
+    {
+    name: "Yu Chien Tang",
+    role: "Hardware",
+    university: "Auburn University, Engineering",
+    photo: "https://eng.auburn.edu/images/image-not-available.jpg",
+    email: "yzt0061@auburn.edu",
+    phone: "3348443532",
+    website: "https://nepcm.eng.auburn.edu/news.php"
+  },
+    {
+    name: "Alyssa Ezell",
+    role: "Hardware",
+    university: "Auburn University, Engineering",
+    photo: "https://eng.auburn.edu/images/image-not-available.jpg",
+    email: "ale0060@auburn.edu",
+    phone: "3348443532",
+    website: "https://nepcm.eng.auburn.edu/news.php"
+  },
+    {
+    name: "Nikith Kandula",
+    role: "Artificial Intelligence",
+    university: "Auburn University, Engineering",
+    photo: "https://eng.auburn.edu/images/image-not-available.jpg",
+    email: "nzk0064@auburn.edu",
+    phone: "3348443532",
+    website: "https://nepcm.eng.auburn.edu/news.php"
+  },
+    {
+    name: "Sai Korrapati",
+    role: "Artificial Intelligence",
+    university: "Auburn University, Engineering",
+    photo: "https://eng.auburn.edu/images/image-not-available.jpg",
+    email: "skk0026@auburn.edu",
+    phone: "3348443532",
+    website: "https://nepcm.eng.auburn.edu/news.php"
+  },
+    {
+    name: "Yusuf Arik",
+    role: "Artificial Intelligence",
+    university: "Auburn University, Engineering",
+    photo: "https://eng.auburn.edu/images/image-not-available.jpg",
+    email: "yea0001@auburn.edu",
+    phone: "3348443532",
+    website: "https://nepcm.eng.auburn.edu/news.php"
+  },
+    {
+    name: "Josh Rice",
+    role: "Artificial Intelligence",
+    university: "Auburn University, Engineering",
+    photo: "https://eng.auburn.edu/images/image-not-available.jpg",
+    email: "jar0175@auburn.edu",
+    phone: "3348443532",
+    website: "https://nepcm.eng.auburn.edu/news.php"
+  },
+];
