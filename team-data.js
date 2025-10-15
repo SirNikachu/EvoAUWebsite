4296091af9038b539fbc39808833d0961e851380
SirNikachu <63741951+SirNikachu@users.noreply.github.com>
2025-10-14 22:45:18 -0500

    Updated theme and reformatted personnel page

--- team-data.js
+++ team-data.js
@@ -1,12 +1,15 @@
 const teamData = [
-    {
+  // Grad Students
+  {
     name: "Faisal Ahmed",
     role: "Graduate Researcher",
     university: "Auburn University, Engineering",
     photo: "https://eng.auburn.edu/images/image-not-available.jpg",
     email: "fza0062@auburn.edu",
     phone: "1234567890",
-    website: "https://nepcm.eng.auburn.edu/news.php"
+    website: "https://nepcm.eng.auburn.edu/news.php",
+    roleCategory: "Grad Students",
+    subRole: ""
   },
   {
     name: "Jack Shanahan",
@@ -15,7 +18,9 @@ const teamData = [
     photo: "https://eng.auburn.edu/images/image-not-available.jpg",
     email: "jts0143@auburn.edu",
     phone: "1234567890",
-    website: "https://nepcm.eng.auburn.edu/news.php"
+    website: "https://nepcm.eng.auburn.edu/news.php",
+    roleCategory: "Grad Students",
+    subRole: ""
   },
   {
     name: "Tommy Kratt",
@@ -24,7 +29,9 @@ const teamData = [
     photo: "https://eng.auburn.edu/images/image-not-available.jpg",
     email: "trk0028@auburn.edu",
     phone: "1234567890",
-    website: "https://nepcm.eng.auburn.edu/news.php"
+    website: "https://nepcm.eng.auburn.edu/news.php",
+    roleCategory: "Grad Students",
+    subRole: ""
   },
   {
     name: "Bikram Bhattarai",
@@ -33,8 +40,12 @@ const teamData = [
     photo: "https://eng.auburn.edu/images/image-not-available.jpg",
     email: "bikram.bhattarai@auburn.edu",
     phone: "1234567890",
-    website: "https://nepcm.eng.auburn.edu/news.php"
+    website: "https://nepcm.eng.auburn.edu/news.php",
+    roleCategory: "Grad Students",
+    subRole: ""
   },
+
+  // Undergrad Students
   {
     name: "Ben Tzou",
     role: "Computational Modeling",
@@ -42,7 +53,9 @@ const teamData = [
     photo: "https://eng.auburn.edu/images/image-not-available.jpg",
     email: "bzt0035@auburn.edu",
     phone: "",
-    website: "https://www.linkedin.com/in/benjamin-tzou/"
+    website: "https://www.linkedin.com/in/benjamin-tzou/",
+    roleCategory: "Undergrad Students",
+    subRole: "Computational Modeling"
   },
   {
     name: "Daniel Vallejo",
@@ -51,78 +64,96 @@ const teamData = [
     photo: "https://eng.auburn.edu/images/image-not-available.jpg",
     email: "dzv0027@auburn.edu",
     phone: "",
-    website: ""
+    website: "",
+    roleCategory: "Undergrad Students",
+    subRole: "Computational Modeling"
   },
-    {
+  {
     name: "George Crocket",
     role: "Computational Modeling",
     university: "Auburn University, Engineering",
     photo: "https://eng.auburn.edu/images/image-not-available.jpg",
     email: "gic0006@auburn.edu",
     phone: "",
-    website: "https://www.linkedin.com/in/george-crockatt/"
+    website: "https://www.linkedin.com/in/george-crockatt/",
+    roleCategory: "Undergrad Students",
+    subRole: "Computational Modeling"
   },
-    {
-    name: "Alexandra Warhurst ",
+  {
+    name: "Alexandra Warhurst",
     role: "Hardware",
     university: "Auburn University, Engineering",
     photo: "https://eng.auburn.edu/images/image-not-available.jpg",
     email: "anw0081@auburn.edu",
     phone: "",
-    website: "https://www.linkedin.com/in/alexandra-warhurst-094980232/"
+    website: "https://www.linkedin.com/in/alexandra-warhurst-094980232/",
+    roleCategory: "Undergrad Students",
+    subRole: "Hardware"
   },
-    {
+  {
     name: "Yu Chien Tang",
     role: "Hardware",
     university: "Auburn University, Engineering",
     photo: "https://eng.auburn.edu/images/image-not-available.jpg",
     email: "yzt0061@auburn.edu",
     phone: "",
-    website: "https://www.linkedin.com/in/yu-chien-tang-90652b299/"
+    website: "https://www.linkedin.com/in/yu-chien-tang-90652b299/",
+    roleCategory: "Undergrad Students",
+    subRole: "Hardware"
   },
-    {
+  {
     name: "Alyssa Ezell",
     role: "Hardware",
     university: "Auburn University, Engineering",
     photo: "https://eng.auburn.edu/images/image-not-available.jpg",
     email: "ale0060@auburn.edu",
     phone: "",
-    website: "https://www.linkedin.com/in/alyssa-ezell-12b68b27a/"
+    website: "https://www.linkedin.com/in/alyssa-ezell-12b68b27a/",
+    roleCategory: "Undergrad Students",
+    subRole: "Hardware"
   },
-    {
+  {
     name: "Nikith Kandula",
     role: "Artificial Intelligence",
     university: "Auburn University, Engineering",
     photo: "https://eng.auburn.edu/images/image-not-available.jpg",
     email: "nzk0064@auburn.edu",
     phone: "",
-    website: "https://www.linkedin.com/in/nik-kandula/"
+    website: "https://www.linkedin.com/in/nik-kandula/",
+    roleCategory: "Undergrad Students",
+    subRole: "Artificial Intelligence"
   },
-    {
+  {
     name: "Sai Korrapati",
     role: "Artificial Intelligence",
     university: "Auburn University, Engineering",
     photo: "https://eng.auburn.edu/images/image-not-available.jpg",
     email: "skk0026@auburn.edu",
     phone: "",
-    website: "https://www.linkedin.com/in/sai-korrapati/"
+    website: "https://www.linkedin.com/in/sai-korrapati/",
+    roleCategory: "Undergrad Students",
+    subRole: "Artificial Intelligence"
   },
-    {
+  {
     name: "Yusuf Arik",
     role: "Artificial Intelligence",
     university: "Auburn University, Engineering",
     photo: "https://eng.auburn.edu/images/image-not-available.jpg",
     email: "yea0001@auburn.edu",
     phone: "",
-    website: "https://www.linkedin.com/in/yusuf-arik-04a946214/"
+    website: "https://www.linkedin.com/in/yusuf-arik-04a946214/",
+    roleCategory: "Undergrad Students",
+    subRole: "Artificial Intelligence"
   },
-    {
+  {
     name: "Josh Rice",
     role: "Artificial Intelligence",
     university: "Auburn University, Engineering",
     photo: "https://eng.auburn.edu/images/image-not-available.jpg",
     email: "jar0175@auburn.edu",
     phone: "",
-    website: "https://www.linkedin.com/in/joshua-rice-102502370/"
-  },
+    website: "https://www.linkedin.com/in/joshua-rice-102502370/",
+    roleCategory: "Undergrad Students",
+    subRole: "Artificial Intelligence"
+  }
 ];
